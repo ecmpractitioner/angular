23ce1d7b3fdcacf2498136e838397763044f0290
ecmpractitione <ecmpractiioner@gmail.com>
2020-06-18 15:35:09 -0500

    selecting comps in a diff way

--- angular-components/src/app/servers/servers.component.ts
+++ angular-components/src/app/servers/servers.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: `<app-server></app-server><app-server></app-server>`,
+  //selector: 'app-servers',
+  //selector:'[app-servers]',//this is similar to css selector way. Add a div in the html to use this componenet
+  selector:'.app-servers',//this is similar to css class. Add this as class attribute in html to use this componenet
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
