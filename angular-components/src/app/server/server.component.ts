4cb0dcffa9a6876cfba3bba0d9701497dca48aa4
ecmpractitione <ecmpractiioner@gmail.com>
2020-06-19 17:29:23 -0500

    styling with ngclass

--- angular-components/src/app/server/server.component.ts
+++ angular-components/src/app/server/server.component.ts
@@ -3,14 +3,33 @@ import {Component} from '@angular/core';
 //whatever you pass within component are the metadata about this class which will be stored
 @Component({
   selector:'app-server',//name should be unique. This is how this componenet is used
-  templateUrl:'./server.component.html' // this holds the html code for this componenet
+  templateUrl:'./server.component.html', // this holds the html code for this componenet
+  styles:[
+    `
+    .online{
+      color:white;
+    }
+  `
+  ]
 })
 export class ServerComponent {
   serverId: number=20;
   serverStatus: string='offline';
 
+  constructor(){
+
+    const random = Math.random();
+    console.log(random);
+    this.serverStatus=random<0.5?"Offline":"Online";
+    
+  }
   getServerStatus(){
     return this.serverStatus;
+  }
+
+  getServerStatusColor(){
+  //debugger;
+    return this.serverStatus==="Offline"?"red":"green";
   }
 
 }
